5e3894c3ee16bc1e5454c3724690b60f74fbdf26
Viktoryia Davydovich <victoriadavidovich13@gmail.com>
2019-06-18 18:10:40 +0300

    post endpoint

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const vd = require("./validation");
+
 const app = new express();
 
 app
   .use(express.static("public"))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .post("/submit", (req, res) => {
+    const userName = req.body.user;
+    const password = req.body.password;
+
+    if (
+      vd.validation.email.test(userName) &&
+      vd.validation.password.test(password)
+    ) {
+      res.send("Success!");
+    } else {
+      res.send("Wrong data!");
+    }
+  })
   .listen(process.env.PORT || 3000, () => console.log("Started"));
